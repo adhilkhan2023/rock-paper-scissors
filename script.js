3d9d69c0f0871684d49543903385c02b8348318e
Adhil Khan <adhilkhan2023@gmail.com>
2024-03-06 14:18:43 +0530

    Create a function to play single round of the game

--- script.js
+++ script.js
@@ -24,3 +24,41 @@ function getComputerChoice(){
 let numberOfTheRound;
 // set the value of numberOfTheRound as 0
 numberOfTheRound = 0;
+
+
+// Create a function playRound
+function playRound(playerSelection,computerSelection){
+  // Increment the variable numberOfTheRound
+  numberOfTheRound++
+
+  // Ask for player's choice and store
+  // it in playerSelection
+  playerSelection = prompt('Rock, Paper Or Scissors?').toLowerCase();
+
+  // Run the getComputerChoice() and store
+  // its value in computerSelection
+  computerSelection = getComputerChoice();
+
+  // if playerSelection is 'rock' and computerSelection is 'paper'
+  if(playerSelection === 'rock' && computerSelection === 'paper' ||
+  // or playerSelection is 'paper' and computerSelection is 'scissors'
+    playerSelection === 'paper' && computerSelection === 'scissors' ||
+  // or playerSelection is 'scissors' and computerSelection is 'rock'
+    playerSelection === 'scissors' && computerSelection === 'rock'
+  // then return `Round ${numberOfTheRound}: You Lose! ${computerSelection} beats ${playerSelection}`
+  ){
+    return `Round ${numberOfTheRound}: You Lose! ${computerSelection} beats ${playerSelection}`
+  // Else if playerSelection is 'paper' and computerSelection is 'rock'
+  }else if(playerSelection === 'paper' && computerSelection === 'rock' ||
+  // or playerSelection is 'scissors' and computerSelection is 'paper'
+    playerSelection === 'scissors' && computerSelection === 'paper' ||
+  // or playerSelection is 'rock' and computerSelection is 'scissors'
+    playerSelection === 'rock' && computerSelection === 'scissors'
+  // then return `Round ${numberOfTheRound}: You Win! ${playerSelection} beats ${computerSelection}`
+  ){
+    return `Round ${numberOfTheRound}: You Win! ${playerSelection} beats ${computerSelection}`
+  // Else return `Round ${numberOfTheRound}: Tie! ${playerSelection} and ${computerSelection}`
+  }else{
+    return `Round ${numberOfTheRound}: Tie! ${playerSelection} and ${computerSelection}`
+  }
+}

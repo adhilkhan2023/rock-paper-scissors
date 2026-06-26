8bb0875de20c9d96913c90621160f2176bf09861
Adhil Khan <adhilkhan2023@gmail.com>
2026-06-26 13:52:25 +0530

    Create function playRound to play a single round

--- js/rock-paper-scissors.js
+++ js/rock-paper-scissors.js
@@ -42,3 +42,49 @@ let humanScore = 0;
 // Create a variable named computerScore.
 // Give the variable computerScore the value 0.
 let computerScore = 0;
+
+
+//Create a function named playRound.
+//Give the playRound function two parameters, humanChoice and computerChoice.
+function playRound(humanChoice, computerChoice){
+  //Convert the value of the humanChoice variable to lowercase.
+  //Store that value in the variable humanChoice.
+  humanChoice = humanChoice.toLowerCase();
+
+  //If the value of the variable humanChoice is rock and the value of the variable computerChoice is scissor
+  //Or the value of the variable humanChoice is paper and the value of the variable computerChoice is rock
+  //Or the value of the variable humanChoice is scissor and the value of the variable computerChoice is paper
+  if(humanChoice === "rock" && computerChoice === "scissors"
+    || humanChoice === "paper" && computerChoice === "rock"
+    || humanChoice === "scissors" && computerChoice === "paper"
+  ){
+    //Increment the value of the variable humanScore by 1.
+    humanScore++;
+    //Then print "You win! ${humanChoice} beats ${computerChoice}".
+    console.log(`You win! ${humanChoice} beats ${computerChoice}`);
+  //Else if the value of the variable humanChoice is scissor and the value of the variable computerChoice is rock   
+  //Or the value of the variable humanChoice is rock and the value of the variable computerChoice is paper
+  //Or the value of the variable humanChoice is paper and the value of the variable computerChoice is scissor
+  }else if(humanChoice === "scissors" && computerChoice === "rock"
+    || humanChoice === "rock" && computerChoice === "paper"
+    || humanChoice === "paper" && computerChoice === "scissors"
+  ){
+    //Increment the value of the variable computerScore by 1.
+    computerScore++;
+    //Then print "You lose! ${computerChoice} beats ${humanChoice}".
+    console.log(`You lose! ${computerChoice} beats ${humanChoice}`);
+    //Else then print "Tie! ${humanChoice} and ${computerChoice}".
+  }else{
+    console.log(`Tie! ${humanChoice} and ${computerChoice}`);
+  }
+}
+ 
+//Create a constant variable humanSelection
+//and assign it the return value of the function getHumanChoice.
+const humanSelection = getHumanChoice();
+//Else then print "Tie! ${humanChoice} and ${computerChoice}".
+//Create a constant variable computerSelection
+//and assign it the return value of the function getComputerChoice.
+const computerSelection = getComputerChoice();
+
+playRound(humanSelection, computerSelection);

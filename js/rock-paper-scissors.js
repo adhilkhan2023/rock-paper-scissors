b4c6959f60d20b2b66e811d796620b261eb55932
Adhil Khan <adhilkhan2023@gmail.com>
2026-07-17 20:28:15 +0530

    Move function playRound out of playGame function
    When the buttons are clicked we need to run the playRound() function,
    if playRound() is inside playGame() function we can't invoke it.

--- js/rock-paper-scissors.js
+++ js/rock-paper-scissors.js
@@ -15,42 +15,43 @@ let humanScore = 0;
 let computerScore = 0;
 
 function playGame(){
-  function playRound(){
-    const humanChoice = getHumanChoice().toLowerCase();
-    const computerChoice = getComputerChoice();
-
-    if(humanChoice === "rock" && computerChoice === "scissors"
-      || humanChoice === "paper" && computerChoice === "rock"
-      || humanChoice === "scissors" && computerChoice === "paper"
-    ){
-      humanScore++;
-      console.log(`You win the round! ${humanChoice} beats ${computerChoice}
-Human Score:${humanScore}   Computer Score:${computerScore}`);
-    }else if(humanChoice === "scissors" && computerChoice === "rock"
-      || humanChoice === "rock" && computerChoice === "paper"
-      || humanChoice === "paper" && computerChoice === "scissors"
-    ){
-      computerScore++;
-      console.log(`You lose the round! ${computerChoice} beats ${humanChoice}
-Human Score:${humanScore}   Computer Score:${computerScore}`);
-    }else{
-      console.log(`Tie! ${humanChoice} and ${computerChoice}
-Human Score:${humanScore}   Computer Score:${computerScore}`);
-    }
-
-    if(humanScore > computerScore){
-      console.log(`You win the game!
-Human Score:${humanScore}   Computer Score:${computerScore}`);
-    }else if(humanScore < computerScore){
-      console.log(`You lose the game!
-Human Score:${humanScore}   Computer Score:${computerScore}`);
-    }else{
-      console.log(`The game is Tie!
-Human Score:${humanScore}   Computer Score:${computerScore}`);
-    }
-  }
 }
 
 playGame();
+
+function playRound(){
+  const humanChoice = getHumanChoice().toLowerCase();
+  const computerChoice = getComputerChoice();
+
+  if(humanChoice === "rock" && computerChoice === "scissors"
+    || humanChoice === "paper" && computerChoice === "rock"
+    || humanChoice === "scissors" && computerChoice === "paper"
+  ){
+    humanScore++;
+    console.log(`You win the round! ${humanChoice} beats ${computerChoice}
+Human Score:${humanScore}   Computer Score:${computerScore}`);
+  }else if(humanChoice === "scissors" && computerChoice === "rock"
+    || humanChoice === "rock" && computerChoice === "paper"
+    || humanChoice === "paper" && computerChoice === "scissors"
+  ){
+    computerScore++;
+    console.log(`You lose the round! ${computerChoice} beats ${humanChoice}
+Human Score:${humanScore}   Computer Score:${computerScore}`);
+  }else{
+    console.log(`Tie! ${humanChoice} and ${computerChoice}
+Human Score:${humanScore}   Computer Score:${computerScore}`);
+  }
+
+  if(humanScore > computerScore){
+    console.log(`You win the game!
+Human Score:${humanScore}   Computer Score:${computerScore}`);
+  }else if(humanScore < computerScore){
+    console.log(`You lose the game!
+Human Score:${humanScore}   Computer Score:${computerScore}`);
+  }else{
+    console.log(`The game is Tie!
+Human Score:${humanScore}   Computer Score:${computerScore}`);
+  }
+}
 
 const buttonsContainer = document.querySelector(".container");
